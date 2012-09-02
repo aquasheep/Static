77568b0d5db7e891d2aeb17b8db776d70459f6f4
Aquasheep <kennepm0@sewanee.edu>
2012-09-01 19:44:48 -0500

    Added applet launcher!

---
+++ Static-Game/src/com/aquasheep/Static/StaticApplet.java
@@ -0,0 +1,14 @@
+package com.aquasheep.Static;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplet;
+
+public class StaticApplet extends LwjglApplet
+{
+	private static final long serialVersionUID = 1L;
+	
+	public StaticApplet()
+	{
+		super(new StaticGame(), false);
+	}
+	
+} 
